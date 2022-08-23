0bed536e52398bee1c20e420b4c9eb12c1e21c43
seventeenxs <110925560+seventeenxs@users.noreply.github.com>
2022-08-23 20:57:10 +0800

    Update Bootstrap.java

--- src/main/java/cn/ofpp/Bootstrap.java
+++ src/main/java/cn/ofpp/Bootstrap.java
@@ -14,17 +14,17 @@ public class Bootstrap {
     /**
      * 公众号AppID
      */
-    public static final String APP_ID = "";
+    public static final String APP_ID = "wxafb518f733cb92a1";
 
     /**
      * 公众号秘钥
      */
-    public static final String SECRET = "";
+    public static final String SECRET = "590ef2001d2601d62824bbc608babe5b";
 
     /**
      * 全局模板ID  也可针对单个Friend指定模板
      */
-    public static final String TEMPLATE_ID = "";
+    public static final String TEMPLATE_ID = "V9PTWvC14AH28gH-9PhJ6IfBboqvBSDAXOpKEVxVjMY";
 
     /**
      * 初始化
