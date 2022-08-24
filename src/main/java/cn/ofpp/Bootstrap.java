f84e9be9d0ee35a1b0a5379f892aee4fa051a04b
fanyy0418 <97722598+fanyy0418@users.noreply.github.com>
2022-08-24 11:38:57 +0800

    Update Bootstrap.java
    配置测试公众号

--- src/main/java/cn/ofpp/Bootstrap.java
+++ src/main/java/cn/ofpp/Bootstrap.java
@@ -14,17 +14,17 @@ public class Bootstrap {
     /**
      * 公众号AppID
      */
-    public static final String APP_ID = "";
+    public static final String APP_ID = "wx4b1c1c286dd705b9";
 
     /**
      * 公众号秘钥
      */
-    public static final String SECRET = "";
+    public static final String SECRET = "9285fa5fa0725060b3c0e50a9117fbba";
 
     /**
      * 全局模板ID  也可针对单个Friend指定模板
      */
-    public static final String TEMPLATE_ID = "";
+    public static final String TEMPLATE_ID = "HXpmNPctwpX0OQKrOi6PT4XZGBFUvj5tUQbN9PXg0R0";
 
     /**
      * 初始化
