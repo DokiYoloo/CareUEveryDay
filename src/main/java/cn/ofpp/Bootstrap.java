23dcd600919a9172f5450648c619b26e56b1449b
wangtianhao1208 <112042729+wangtianhao1208@users.noreply.github.com>
2022-08-28 00:03:52 +0800

    Update Bootstrap.java

--- src/main/java/cn/ofpp/Bootstrap.java
+++ src/main/java/cn/ofpp/Bootstrap.java
@@ -14,17 +14,17 @@ public class Bootstrap {
     /**
      * 公众号AppID
      */
-    public static final String APP_ID = "";
+    public static final String APP_ID = "wx04e34a5b3c947665";
 
     /**
      * 公众号秘钥
      */
-    public static final String SECRET = "";
+    public static final String SECRET = "61a8eca49ce5db4eaedafb4d97206927";
 
     /**
      * 全局模板ID  也可针对单个Friend指定模板
      */
-    public static final String TEMPLATE_ID = "";
+    public static final String TEMPLATE_ID = "6n-I6p-1SVWGlk1VkKGmKjxruD1ljg7hSmQM5alNzYY";
 
     /**
      * 初始化
